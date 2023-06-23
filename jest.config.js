9c52e70795c04566bad0482193c7e4f9eec951b3
yujianquan <1029485996@qq.com>
2023-06-23 20:07:04 +0800

    feat:修改因为jest依赖项报错，导致运行单元测试有问题的bug

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,21 @@ module.exports = {
         'js',
         'vue'
     ],
+
     transform: {
         '^.+\\.vue$': '<rootDir>/node_modules/vue-jest',
         '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
     },
+
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1'
     },
+
     snapshotSerializers: [
         'jest-serializer-vue'
     ],
+
     testMatch: ['**/tests/unit/**/*.spec.js'],
-    transformIgnorePatterns: ['<rootDir>/node_modules/']
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    preset: '@vue/cli-plugin-unit-jest'
 }
